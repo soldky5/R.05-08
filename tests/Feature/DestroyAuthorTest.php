c64b063b841d17a2d64ea56aea9ffb163fb4aa2d
Vincent Labarre <vincent.labarre@localnova.fr>
2025-09-10 10:32:52 +0200

    add new test

---
+++ tests/Feature/DestroyAuthorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexAuthorTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_destroy_author(): void
+    {
+        // TODO 2 : Étape RED, écrire un test pour vérifier que la suppression renvoie bien une 204 (HTTP: no content)
+    }
+    // TODO 4 : Étape REFACTOR, ajouter un test pour vérifier que l'auteur a bien été supprimé de la BDD
+}
